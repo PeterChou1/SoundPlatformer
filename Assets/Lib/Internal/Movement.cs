ae7bdb598e2b3cc24c615bf70c1c5b47747b54e5
Fasith-23 <89504643+Fasith-23@users.noreply.github.com>
2022-09-21 00:07:36 +0530

    popUp&inspector

--- Assets/Lib/Internal/Movement.cs
+++ Assets/Lib/Internal/Movement.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Movement : MonoBehaviour
 {
     public float speed = 2f;
     public Camera Cam2D,Cam3D;
+    public Animator animator;
+    
+    private bool sit = true;
+    private bool play = true;
+    public TMP_Text popUpText;
+    public Image popUpBg;
     private void Start()
     {
         Cam3D.enabled = true;
         Cam2D.enabled = false;
+        popUpText.enabled = false;
+        popUpBg.enabled = false;
+
+
     }
 
     void Update()
@@ -30,6 +42,17 @@ public class Movement : MonoBehaviour
         if (Input.GetMouseButtonDown(1))
         {
             // trigger click animation
+            animator.SetBool("isPlaying", play);
+            if (play == false)
+            {
+                popUpText.text = "You left the game";
+            }
+        
+
+
+            StartCoroutine(waiter());
+            
+            play = !play;
             // switch to 2D
             Cam3D.enabled = !Cam3D.enabled;
             Cam2D.enabled = !Cam2D.enabled;
@@ -43,30 +66,58 @@ public class Movement : MonoBehaviour
 
         if (other.gameObject.tag == "seat")
         {
+           
             if (Input.GetMouseButtonDown(0))
             {
+                
                 // trigger sit animation
+                animator.SetBool("isSitting", sit);
+                popUpText.text = "You are sitting";
+                StartCoroutine(waiter());
+
+                //message pop up
+                popUpText.text = "You are sitting";
+                StartCoroutine(waiter());
+                //Debug.Log("howdy");
+                sit = !sit;
             }
         }
         if (other.gameObject.tag == "driver")
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButton(0))
             {
                 // trigger wave animation
+                animator.SetBool("isWaving", true);
+                popUpText.text = "You are waving";
+                StartCoroutine(waiter());
             }
+            /*if (Input.GetMouseButtonUp(0))
+            {
+                animator.SetBool("isWaving", false);
+            }*/
         }
         if (other.gameObject.tag == "door")
         {
             if (Input.GetMouseButtonDown(0))
             {
                 // trigger leave animation
+                animator.SetBool("isLeaving", true);
+                popUpText.text = "You left the bus";
+                StartCoroutine(waiter());
             }
         }
-        if (other.gameObject.tag == "phone")
-        {
-          
-        }
+        
 
+    }
+    IEnumerator waiter()
+    {
+        popUpText.enabled = !popUpText.enabled;
+        popUpBg.enabled = !popUpBg.enabled;
+
+        yield return new WaitForSecondsRealtime(2);
+
+        popUpText.enabled = !popUpText.enabled;
+        popUpBg.enabled = !popUpBg.enabled;
     }
 
 
